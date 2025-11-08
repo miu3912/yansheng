27d3d18e0b82891aaa1d3aa7485291bb4598d3c6
miu3912 <cooannabelle@gmail.com>
2025-11-08 18:22:49 +0800

    Update 主界面.vue

--- src/哥布林巢穴-简版/界面显示层/主界面.vue
+++ src/哥布林巢穴-简版/界面显示层/主界面.vue
@@ -4,7 +4,7 @@
       <div class="content-wrapper">
         <header class="header">
           <div class="header-center">
-            <h1 class="main-title">哥布林巢穴</h1>
+            <h1 class="main-title">衍生之圣巢</h1>
           </div>
         </header>
 
